704304af8e68e5e3058570d5299ea8cd0cc684a0
Chirag131 <chiragtuteja2006@gmail.com>
2025-05-05 02:33:02 +0530

    Video Capture added

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,9 +1,17 @@
-from flask import Blueprint,render_template,url_for,request
+from flask import Blueprint,render_template,url_for,request,Response
+from ..functions.generateFrames import generate_frames
 from ..models import *
 from .. import db
 
 views = Blueprint('views' , __name__)
 
+
 @views.route('/')
 def index():
-    return "<h1>HI</h1>"
+    return render_template('index.html')
+
+@views.route('/video')
+def video_feed():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
